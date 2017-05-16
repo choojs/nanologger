d7bd65e47b44955eefca1776dc44f33ff5a082e0
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-05-16 13:43:41 +0200

    allow arbitrary length inputs

--- index.js
+++ index.js
@@ -1,84 +1,108 @@
-var emojis = {
-  debug: '🐛',
-  info: '✨',
-  warn: '⚠️',
-  error: '🚨',
-  fatal: '💀'
-}
-
-var levels = {
-  debug: 20,
-  info: 30,
-  warn: 40,
-  error: 50,
-  fatal: 60
-}
-
-var colors = {
-  foreground: '#d3c0c8',
-  background: '#2d2d2d',
-  black: '#2d2d2d',
-  red: '#f2777a',
-  green: '#99cc99',
-  yellow: '#ffcc66',
-  blue: '#6699cc',
-  magenta: '#cc99cc',
-  cyan: '#66cccc',
-  white: '#d3d0c8',
-  brightBlack: '#747369'
-}
-
 module.exports = Nanologger
 
 function Nanologger (name) {
   if (!(this instanceof Nanologger)) return new Nanologger(name)
+
   this._name = name || ''
+
+  this.colors = {
+    foreground: '#d3c0c8',
+    background: '#2d2d2d',
+    black: '#2d2d2d',
+    red: '#f2777a',
+    green: '#99cc99',
+    yellow: '#ffcc66',
+    blue: '#6699cc',
+    magenta: '#cc99cc',
+    cyan: '#66cccc',
+    white: '#d3d0c8',
+    brightBlack: '#747369'
+  }
+
+  this.emojis = {
+    debug: '🐛',
+    info: '✨',
+    warn: '⚠️',
+    error: '🚨',
+    fatal: '💀'
+  }
+
+  this.levels = {
+    debug: 20,
+    info: 30,
+    warn: 40,
+    error: 50,
+    fatal: 60
+  }
+
   try {
     this.logLevel = window.localStorage.getItem('logLevel') || 'info'
   } catch (e) {
     this.logLevel = 'info'
   }
-  this._logLevel = levels[this.logLevel]
+
+  this._logLevel = this.levels[this.logLevel]
 }
 
-Nanologger.prototype.debug = function (message, data) {
-  this._print('debug', message, data)
+Nanologger.prototype.debug = function () {
+  var args = [ 'debug' ]
+  for (var i = 0, len = arguments.length; i < len; i++) args.push(arguments[i])
+  this._print.apply(this, args)
 }
 
-Nanologger.prototype.info = function (message, data) {
-  this._print('info', message, data)
+Nanologger.prototype.info = function () {
+  var args = [ 'info' ]
+  for (var i = 0, len = arguments.length; i < len; i++) args.push(arguments[i])
+  this._print.apply(this, args)
 }
 
-Nanologger.prototype.warn = function (message, data) {
-  this._print('warn', message, data)
+Nanologger.prototype.warn = function () {
+  var args = [ 'warn' ]
+  for (var i = 0, len = arguments.length; i < len; i++) args.push(arguments[i])
+  this._print.apply(this, args)
 }
 
-Nanologger.prototype.error = function (message, data) {
-  this._print('error', message, data)
+Nanologger.prototype.error = function () {
+  var args = [ 'error' ]
+  for (var i = 0, len = arguments.length; i < len; i++) args.push(arguments[i])
+  this._print.apply(this, args)
 }
 
-Nanologger.prototype.fatal = function (message, data) {
-  this._print('fatal', message, data)
+Nanologger.prototype.fatal = function () {
+  var args = [ 'fatal' ]
+  for (var i = 0, len = arguments.length; i < len; i++) args.push(arguments[i])
+  this._print.apply(this, args)
 }
 
-Nanologger.prototype._print = function (level, message, data) {
-  if (levels[level] < this._logLevel) return
+Nanologger.prototype._print = function (level) {
+  if (this.levels[level] < this._logLevel) return
 
-  data = data === undefined ? '' : data
-  data = data || ''
+  var colors = this.colors
   var time = this._getTimeStamp()
-  var emoji = emojis[level]
+  var emoji = this.emojis[level]
   var name = this._name || 'unknown'
   var c = this._c
 
   var msgColor = (level === 'error' || level.fatal)
     ? colors.red
-    : (level === 'warn')
+    : level === 'warn'
       ? colors.yellow
       : colors.green
 
-  var msg = '%c' + time + ' ' + emoji + ' %c' + name + ' %c' + message
-  console.log(msg, c(colors.brightBlack), c(colors.magenta), c(msgColor), data)
+  var msg = '%c' + time + ' ' + emoji + ' %c' + name
+  var args = [msg, c(colors.brightBlack), c(colors.magenta)]
+
+  for (var i = 1, len = arguments.length; i < len; i++) {
+    var arg = arguments[i]
+    if (i === 1 && typeof arg === 'string') {
+      args[0] = msg + ' %c' + arg
+      args.push(c(msgColor))
+    } else {
+      args.push(arg)
+    }
+  }
+
+  console.log.apply(console, args)
 }
 
 Nanologger.prototype._c = function (color) {
